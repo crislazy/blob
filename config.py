b6afb2470792dba284b09860a9a7dffdf4b9e3cc
crislazy <271609449+crislazy@users.noreply.github.com>
2026-04-19 12:39:16 +0300

    first commit

---
+++ config.py
@@ -0,0 +1,46 @@
+# ==== Config.py ====
+
+# ==== TOKEN ====
+bot_secret = "placeholder123"
+
+# ==== ROLES ====
+mod_roles_id = [123456789] # Moderator roles
+member_role_id = 123456789 # The role that is automaticly given to new members
+
+# ==== CHANNELS ====
+welcome_channel_id = 123456789 # Where it sends a welcome message on join/leave
+
+# ==== FEATURES ====
+# Toggle Features(True = On ; False = Off)
+
+ENABLE_WELCOME_MESSAGE = True # Send a message on join
+ENABLE_GOODBYE_MESSAGE = True # Send a message on leave
+AUTO_ROLE = True # Gives a role on join
+PING_RESPOND = True # Responds with a message when pinged
+SEND_AS_BOT = True # Send a message as the bot
+CUSTOM_STATUS = True # Have a Custom Status
+STATUS_TEXT = "Mining In BlobCraft" # Status Text
+STATUS_TYPE = "playing"  # playing / watching / listening
+HELP_COMMAND = True
+
+# ==== MESSAGES ====
+welcome_messages = [
+    "{user} boarded the plane",
+    "{user} joined the server",
+    "{user} got teleported here"
+]
+
+goodbye_messages = [
+    "{user} jumped off the plane",
+    "{user} got kicked off the plane",
+    "{user} left",
+    "{user} had a stroke",
+    "{user} might have been kicked, idk tbh"
+]
+
+blob_messages = [
+    "Yes?",
+    "No",
+    ":3",
+    "This is a response to your message, please shut up"
+]
---
+++ main.py
@@ -0,0 +1,132 @@
+# BLOB - A Discord Bot
+
+import discord
+from discord.ext import commands
+import random
+import config
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+    if config.CUSTOM_STATUS:
+        activity = None
+
+        if config.STATUS_TYPE == "playing":
+            activity = discord.Game(name=config.STATUS_TEXT)
+        elif config.STATUS_TYPE == "watching":
+            activity = discord.Activity(type=discord.ActivityType.watching, name=config.STATUS_TEXT)
+        elif config.STATUS_TYPE == "listening":
+            activity = discord.Activity(type=discord.ActivityType.listening, name=config.STATUS_TEXT)
+
+        await bot.change_presence(activity=activity)
+
+#Welcome Message Announcement
+@bot.event
+async def on_member_join(member):
+    if not config.ENABLE_WELCOME_MESSAGE:
+        return
+    channel = bot.get_channel(config.welcome_channel_id)
+    if config.AUTO_ROLE: 
+        role = member.guild.get_role(config.member_role_id)
+        if role:
+            await member.add_roles(role)
+    if channel:
+        msg = random.choice(config.welcome_messages).format(user=member.mention)
+        await channel.send(msg)
+
+#Goodbye Message Announcement
+@bot.event
+async def on_member_remove(member):
+    if not config.ENABLE_GOODBYE_MESSAGE:
+        return
+    channel = bot.get_channel(config.welcome_channel_id)
+
+    if channel is None:
+        return
+
+    try:
+        msg = random.choice(config.goodbye_messages).format(user=member.name)
+        await channel.send(msg)
+    except discord.Forbidden:
+        print("Bot cannot send messages in this channel")
+
+#Ping-Pong
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f"Pong {round(bot.latency * 1000)}ms")
+
+#Kick
+@bot.command()
+@commands.has_permissions(kick_members=True)
+async def kick(ctx, member: discord.Member, *, reason=None):
+    try:
+        await member.kick(reason=reason)
+        await ctx.send(f"{member} has been kicked. Reason: {reason}")
+    except discord.Forbidden:
+        await ctx.send("User had no perms")
+    except Exception as e:
+        await ctx.send(f"Error: {e}")
+    await ctx.message.delete()
+
+#Ban
+@bot.command()
+@commands.has_permissions(ban_members=True)
+async def ban(ctx, member: discord.Member, *, reason=None):
+    try:
+        await member.ban(reason=reason)
+        await ctx.send(f"{member} has been banned. Reason: {reason}")
+    except discord.Forbidden:
+        await ctx.send("User had no perms")
+    except Exception as e:
+        await ctx.send(f"Error: {e}")
+    await ctx.message.delete()
+
+#Get a response on ping
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if config.PING_RESPOND and bot.user in message.mentions:
+        await message.channel.send(random.choice(config.blob_messages))
+
+    await bot.process_commands(message)
+
+#Send a message as bot
+@bot.command()
+async def s(ctx, channel_id: int, *, message):
+    if not config.SEND_AS_BOT:
+        await ctx.send("❌ This feature is disabled.")
+        return
+    # check roles
+    if not any(role.id in config.mod_roles_id for role in ctx.author.roles):
+        await ctx.message.delete()
+        return
+
+    channel = bot.get_channel(channel_id)
+    if channel is None:
+        await ctx.send("Channel not found.")
+        return
+
+    await channel.send(message)
+
+#Help
+@bot.command()
+async def help(ctx):
+    if not config.HELP_COMMAND:
+        return
+    await ctx.send("""
+    🛠 Commands:
+    !ping - check latency
+    !kick - kick a user (mods only)
+    !ban - ban a user (mods only)
+    !s - send message as bot (mods only)
+    !help - THIS COMMAND
+    """)
+
+bot.run(config.bot_secret)
